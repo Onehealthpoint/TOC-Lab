27077a6628aeb1a2ba7cb95147f2ca0a930fab96
Onehealthpoint <143315866+Onehealthpoint@users.noreply.github.com>
2024-04-11 17:11:57 +0545

    Add files via upload

---
+++ lab 3.cpp
@@ -0,0 +1,70 @@
+/*
+String has even number of a's and b's over E = {a, b}
+*/
+
+#include<iostream>
+using namespace std;
+
+void q0(string, int);
+void q1(string, int);
+void q2(string, int);
+void q3(string, int);
+void q4(string, int);
+
+void q0(string s, int i){
+	if(i==s.length()){
+		cout<<"Ok.The string is not  accepted\n";
+		return;
+	}
+	if(s[i] =='a')
+	q1(s,i+1);
+	else 
+	q3(s,i+1);
+}
+
+void q1(string s, int i){
+	if(i==s.length()){
+		cout<<"Ok.The string is accepted\n";
+		return;
+	}
+	if(s[i] =='a')
+	q1(s,i+1);
+	else 
+	q2(s,i+1);
+}
+void q2(string s, int i){
+		if(i ==s.length()){
+			cout<<"No.The string is not accepted\n";
+			return;
+		}
+		if(s[i]=='a')
+		q1(s,i+1);
+		else q2(s,i+1);
+	}
+void q3(string s, int i){
+		if(i ==s.length()){
+			cout<<"Ok.The string is accepted\n";
+			return;
+		}
+		if(s[i] == 'a')
+		q4(s,i+1);
+		else q3(s,i+1);
+	}
+void q4(string s, int i){
+		if(i ==s.length()){
+			cout<<"No.The string is not accepted\n";
+			return;
+		}
+		if(s[i] == 'a')
+		q4(s,i+1);
+		else q3(s,i+1);
+	}
+int main(){
+	string s;
+	 cout<<"Enter the string\n";
+	 cin>>s;
+	 q0(s,0);
+	 return 0;
+}
+
+
